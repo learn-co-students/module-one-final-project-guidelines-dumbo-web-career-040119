ff92687d062cc39d9f5b7bd7fad12423bb0fb081
github username <github email address>
2019-04-15 16:44:24 -0400

    etc2

--- lib/directory.rb
+++ lib/directory.rb
@@ -3,7 +3,6 @@ class Directory < ActiveRecord::Base
   belongs_to :user
 
 
-
   def auto_directory(new_dir)
     starting_tip_id = Tip.where('name = "Golden Rule"')[0].id
     new_dir.tip_id = starting_tip_id
@@ -14,6 +13,7 @@ class Directory < ActiveRecord::Base
     Tip.where("id = #{self.tip_id}")[0].content
   end
 
+
   def get_user_labels(user)
     counter = 0
     all_users_tips = Directory.where("user_id = ?", user.id)
@@ -22,51 +22,42 @@ class Directory < ActiveRecord::Base
   end
 
 
-def edit_label(user)
-  prompt = TTY::Prompt.new
-  new_label = prompt.ask("What would you like to update your label as?")
-  self.update(label: new_label)
-puts "Your label has been updated to #{new_label}."
-user_home_page(user)
-end
-
-
-def delete_label(user)
-  prompt = TTY::Prompt.new
-gets = prompt.select("Are you sure?", %w[Yes No])
-  if gets == "Yes"
-    self.delete
-puts "This tip has been deleted"
-sleep 2
+  def edit_label(user)
+    prompt = TTY::Prompt.new
+    new_label = prompt.ask("What would you like to update your label as?")
+    self.update(label: new_label)
+    puts "Your label has been updated to #{new_label}."
+    user_home_page(user)
   end
-  user_home_page(user)
-end
 
 
+  def delete_label(user)
+    prompt = TTY::Prompt.new
+    gets = prompt.select("Are you sure?", %w[Yes No])
+    if gets == "Yes"
+      self.delete
+      puts "This tip has been deleted"
+      sleep 2
+    end
+    user_home_page(user)
+  end
 
 
-def display_and_edit_tip(tip, user)
+  def display_and_edit_tip(tip, user)
+    system "clear"
+    prompt = TTY::Prompt.new
+    puts tip.name
+    puts tip.content
+    nav = prompt.select(" ", %w[Edit Delete Back])
 
-system "clear"
-  prompt = TTY::Prompt.new
-  puts tip.name
-  puts tip.content
-  nav = prompt.select(" ", %w[Edit Delete Back])
-
-  if nav == "Edit"
-
-  edit_label(user)
-
-  elsif nav == "Delete"
-
-  delete_label(user)
-
-else
-
-user_saved_tips(user)
-
-end
-
+    if nav == "Edit"
+      edit_label(user)
+    elsif nav == "Delete"
+      delete_label(user)
+    else
+      user_saved_tips(user)
+    end
+  end
 
 
   def user_saved_tips(user)
@@ -77,28 +68,24 @@ end
     # binding.pry
     nav = prompt.select("These are your saved labels", labels)
 
-if nav == "Back"
-  user_home_page(user)
-else
-  your_chosen_label = nav.split(". ")[1]
-  the_labels = Directory.where("label = ?", your_chosen_label)
+    if nav == "Back"
+      user_home_page(user)
+    else
+      your_chosen_label = nav.split(". ")[1]
+      the_labels = Directory.where("label = ?", your_chosen_label)
 
-  counter = 0
-  choices = the_labels.map do |user_dir|
-    # binding.pry
-  tip = Tip.where("id = ?", user_dir.tip_id)[0]
-  # binding.pry
-"#{counter += 1}. #{tip.content}"
+      counter = 0
+      choices = the_labels.map do |user_dir|
+        tip = Tip.where("id = ?", user_dir.tip_id)[0]
+        "#{counter += 1}. #{tip.content}"
+      end
 
- end
-
- prompt = TTY::Prompt.new
-
- choice = prompt.select(' ', choices).split('. ')
+      prompt = TTY::Prompt.new
+      choice = prompt.select(' ', choices).split('. ')
       tip = Tip.where('content = ?', choice[1])
       display_and_edit_tip(tip, user)
-
-end
+    end
+  end
 
  #check whether displayed labels belong to a single user
 #check whether the displayed labels are in fact all the labels that this user has
@@ -122,5 +109,3 @@ end
   end
 
 end
-
-end 
