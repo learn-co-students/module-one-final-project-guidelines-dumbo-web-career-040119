ad934318c44e8720c745b92fb7a019d0059f0170
github username <github email address>
2019-04-15 16:33:03 -0400

    etc

--- lib/directory.rb
+++ lib/directory.rb
@@ -2,6 +2,8 @@ class Directory < ActiveRecord::Base
   belongs_to :tip
   belongs_to :user
 
+
+
   def auto_directory(new_dir)
     starting_tip_id = Tip.where('name = "Golden Rule"')[0].id
     new_dir.tip_id = starting_tip_id
@@ -18,6 +20,54 @@ class Directory < ActiveRecord::Base
     users_labels = all_users_tips.map(&:label).uniq
     users_labels.map {|label| "#{counter += 1}. #{label}"}
   end
+
+
+def edit_label(user)
+  prompt = TTY::Prompt.new
+  new_label = prompt.ask("What would you like to update your label as?")
+  self.update(label: new_label)
+puts "Your label has been updated to #{new_label}."
+user_home_page(user)
+end
+
+
+def delete_label(user)
+  prompt = TTY::Prompt.new
+gets = prompt.select("Are you sure?", %w[Yes No])
+  if gets == "Yes"
+    self.delete
+puts "This tip has been deleted"
+sleep 2
+  end
+  user_home_page(user)
+end
+
+
+
+
+def display_and_edit_tip(tip, user)
+
+system "clear"
+  prompt = TTY::Prompt.new
+  puts tip.name
+  puts tip.content
+  nav = prompt.select(" ", %w[Edit Delete Back])
+
+  if nav == "Edit"
+
+  edit_label(user)
+
+  elsif nav == "Delete"
+
+  delete_label(user)
+
+else
+
+user_saved_tips(user)
+
+end
+
+
 
   def user_saved_tips(user)
     prompt = TTY::Prompt.new
@@ -43,8 +93,11 @@ else
  end
 
  prompt = TTY::Prompt.new
- prompt.select(" ", choices)
- 
+
+ choice = prompt.select(' ', choices).split('. ')
+      tip = Tip.where('content = ?', choice[1])
+      display_and_edit_tip(tip, user)
+
 end
 
  #check whether displayed labels belong to a single user
@@ -52,7 +105,6 @@ end
 #
 
 
-  end
 
   def user_home_page(user)
     prompt = TTY::Prompt.new
@@ -70,3 +122,5 @@ end
   end
 
 end
+
+end 
