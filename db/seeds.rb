7d4db35b6ac037fb7949464db391298af582e765
ftse105 <ftse105@gmail.com>
2019-04-15 14:41:58 -0400

    three

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@ adidas_pants = Clothing.create(name: "Track", material: "Polyester", price: 44.9
 nike_shoes = Clothing.create(name: "Flyknits", material: "Cotton", price: 105.00)
 nike_hoodie = Clothing.create(name: "Tech Fleece", material: "Cotton", price: 99.00)
 nike_pants = Clothing.create(name: "Sweatpants", material: "Polyester", price: 45.50)
+
+nike = Brand.create(name: "Nike", pricepoint: "luxury")
+adidas = Brand.create(name: "Adidas", pricepoint: "luxury")
+ua = Brand.create(name: "Under Armour", pricepoint: "budget")
+
+shoes = Aritcle.create(name: "Shoes")
+shirts = Article.create(name: "Shirts")
+pants = Article.create(name: "Pants")
