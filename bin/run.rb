3cd9edef230aba095ea071e86e4c6df88eb4f4ee
github username <github email address>
2019-04-17 09:27:50 -0400

    Stable Experimental
    Experimental branch with new stabilized features.

--- bin/run.rb
+++ bin/run.rb
@@ -2,37 +2,99 @@ require_relative '../config/environment'
 require_relative 'methods'
 # require_all './models'
 
+def welcome
+  puts "Welcome user!"
+  get_feedback
+end
 
-puts "Welcome user. If you're interested in some threads, you've come to the right place. Talk to me about what's important to you."
-
+def get_feedback
+  puts "If you're interested in some threads, you've come to the right place. Talk to me about what's important to you."
 #user has the opportunity to make a freeform input and the program will search for certain keywords to determine its course. If none are found, proceeds to its default course.
-first_question_input = gets.chomp
+  first_question_input = gets.chomp
+  input_analyzer_first_question(first_question_input)
+end
 
+def input_analyzer_first_question(string)
+  if string.include? "price"
+    analyze_feedback_budget
+  elsif string.include? "style"
+    analyze_feedback_luxury
+  elsif string.include? "brand"
+    return answer = "brand"
+  else
+    return answer = "default"
+  end
+end
 #user makes a choice of what slot of clothing they want (pants, shoes, etc.) to provide the program with the second search criteria
-if input_analyzer_first_question(first_question_input) == "budget"
-  puts "No problem, let's get you stuff that'll leave your wallet happy."
-  sleep(1)
-  puts "What kind of clothes are we looking for?"
-  articles_question_input = gets.chomp
-  puts "All right, how about #{Brand.choose_budget(articles_question_input).name}? Only costs $#{Brand.choose_budget(articles_question_input).price}."
+def analyze_feedback_budget
+    puts "No problem, let's get you stuff that'll leave your wallet happy."
+    sleep(1)
+    suggestion = Brand.choose_budget(articles_question)
+    puts "All right, how about #{suggestion.name}? Only costs $#{suggestion.price}."
+    exit_or_retry?
+end
 
-elsif input_analyzer_first_question(first_question_input) == "luxury"
-  puts "No problem, let's get you looking fancy."
-  sleep(1)
-  puts "What kind of clothes are we looking for?"
-  articles_question_input = gets.chomp
-  puts "All right, how about #{Brand.choose_luxury(articles_question_input).name}? They're worth $#{Brand.choose_luxury(articles_question_input).price}."
+def analyze_feedback_luxury
+    puts "No problem, let's get you looking fancy."
+    sleep(1)
+    suggestion = Brand.choose_luxury(articles_question)
+    puts "All right, how about #{suggestion.name}? They're worth $#{suggestion.price}."
+    exit_or_retry?
+end
 
-elsif input_analyzer_first_question(first_question_input) == "brand"
+def analyze_feedback_random
+    puts "Maybe you're having a hard time choosing? Here, let me throw out a random suggestion!"
+    random_selection = Clothing.random
+    puts "How's #{random_selection.name}? They're #{random_selection.price}."
+    exit_or_retry?
+end
+
+def analyze_feedback_brand
   puts "Want something with a label on it?"
-  puts "Whose clothes are we looking for?"
-  articles_question_input = gets.chomp
-  puts "All right, any these flashy enough for you?"
+  brand_question
+  sleep(1)
+  puts "All right, any of these flashy enough for you?"
   Brand.choose_brand(articles_question_input).each do |element|
     puts element.name
   end
-else input_analyzer_first_question(first_question_input) == "default"
-  puts "Maybe you're having a hard time choosing? Here, let me throw out a random suggestion!"
-  random_selection = Clothing.random
-  puts "How's #{random_selection.name}? They're #{random_selection.price}. Made of some nice #{random_selection.material}."
 end
+
+def articles_question
+  prompt = TTY::Prompt.new
+  sleep(1)
+    prompt.select("What kind of clothes are we looking for?") do |menu|
+      menu.choice 'Shirts'
+      menu.choice 'Pants'
+      menu.choice 'Shoes'
+    end
+end
+
+def brand_question
+  prompt = TTY::Prompt.new
+  prompt.select("Whose clothes are we wearing?")
+    menu.choice 'Adidas'
+    menu.choice 'Nike'
+    menu.choice 'Reebok'
+    menu.choice 'Under Armor'
+end
+
+
+def exit_or_retry?
+  prompt = TTY::Prompt.new
+  prompt.select("Want to go again, or are you done for now?") do |menu|
+    menu.choice 'Try Again', -> {new_round}
+    menu.choice 'Exit', -> {exit}
+  end
+end
+
+def new_round
+ "Looking for more stuff? I got you. Talk to me again."
+  first_question_input = gets.chomp
+  input_analyzer_first_question(first_question_input)
+end
+
+def exit
+  "Hope I could help and hope you leave looking sharp."
+end
+
+welcome
--- lib/models/brand.rb
+++ lib/models/brand.rb
@@ -2,16 +2,16 @@ class Brand < ActiveRecord::Base
   has_many :clothings
   has_many :articles, through: :clothings
 
-  def self.choose_budget(articles_question_input)
+  def self.choose_budget(articles_question)
     brand_pricepoints = self.all.select{|brand| brand.pricepoint == "budget"}
     budget_brands_clothings = brand_pricepoints.map{|brand| brand.clothings}
-    budget_brands_clothings.flatten.find{|clothing| clothing.article.slot == articles_question_input}
+    budget_brands_clothings.flatten.find{|clothing| clothing.article.slot == articles_question}
   end
 
-  def self.choose_luxury(articles_question_input)
+  def self.choose_luxury(articles_question)
     brand_pricepoints = self.all.select{|brand| brand.pricepoint == "luxury"}
-    budget_brands_clothings = brand_pricepoints.map{|brand| brand.clothings}
-    budget_brands_clothings.flatten.find{|clothing| clothing.article.slot == articles_question_input}
+    luxury_brands_clothings = brand_pricepoints.map{|brand| brand.clothings}
+    luxury_brands_clothings.flatten.find{|clothing| clothing.article.slot == articles_question}
   end
 
   def self.choose_brand(articles_question_input)
